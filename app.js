d566b961b21ce8d2edea360cc617db572c815037
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-07-01 20:37:32 -0700

    Complete Brush size

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
 
 ctx.strokeStyle = "black";
-ctx.linewidth = 2.5;
+ctx.lineWidth = 2.5;
 let paint = false;
 canvas.width = 500;
 canvas.height = 500;
@@ -26,12 +27,21 @@ function onMouseMove(event) {
         ctx.lineTo(x, y);
         ctx.stroke();
     }
+    if (range) {
+        range.addEventListener("input", handleRange);
+    }
 
 }
 
 function changeColor(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+}
+
+function handleRange(event) {
+
+    const size = event.target.value;
+    ctx.lineWidth = size;
 
 }
 
@@ -41,6 +51,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPaint);
     canvas.addEventListener("mouseleave", stopPaint);
 }
+
 Array.from(colors).forEach(color => {
     color.addEventListener("click", changeColor);
 
