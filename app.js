b131fd02216d55bb8161d3c2ed3873cc8d3596a9
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-07-01 17:35:13 -0700

    add index.html, style.css, reset.css

---
+++ app.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("jsCanvas");
+
+
+
+function onMouseMove(event) {
+    console.log(event);
+}
+
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove)
+}
+
+
+
+
+
+
+function init() {
+
+}
+init();
